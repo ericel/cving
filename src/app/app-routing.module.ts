1ce01b0109ef2415a3728a61d986d909b488c62a
Oj Obasi <ericel123new@gmail.com>
2016-10-06 21:51:19 +0900

    Home page redirect to Home Component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,21 @@
 import { ModuleWithProviders }  from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 import { AppComponent } from './main/app.component';
 import { AssetsComponent } from './assets/assets.component';
 
 const routes: Routes = [
    {
-    path: '',
-    component: AppComponent,
+    path: 'home',
+    component: HomeComponent,
     data: {
       title: 'Submit your cv and wait'
     }
+  },
+  {
+   path: '',
+   redirectTo: '/home',
+   pathMatch: 'full'
   },
    {
     path: 'assets',
